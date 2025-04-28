107f7f11b3970c36446f14a17f7039727f31118a
Edwin Plauchu <j4nusx@yahoo.com>
2025-04-28 17:05:03 -0500

    performance

--- sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Vehicle.java
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Vehicle.java
@@ -1,12 +1,22 @@
 package com.agnux.haul.repository.model;
 
+import java.math.BigDecimal;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class Vehicle extends TmsModel {
 
     private String numberPlate;
     private VehicleType vehicleType;
+
+    // El rendimiento es una media de los históricos
+    // Algunos vehiculos caracterizan una interface electronica
+    // para obtener rendimiento directamente del motor
+    // El rendimiento es una media de los históricos
+    private DistUnit performanceUnit;
+    private BigDecimal performanceScalar;
 
     public Vehicle(final String vehicleId, final String tenantId, String numberPlate, VehicleType vehicleType) {
         super(vehicleId, tenantId);
