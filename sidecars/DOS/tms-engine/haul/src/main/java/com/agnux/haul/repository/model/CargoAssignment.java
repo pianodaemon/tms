d2399dfa98934c6a016982e81d05972e7409560e
Edwin Plauchu <j4nusx@yahoo.com>
2025-04-28 21:30:14 -0500

    geolocalization

--- sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/CargoAssignment.java
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/CargoAssignment.java
@@ -8,6 +8,10 @@ public class CargoAssignment extends TmsBasicModel {
     private Vehicle vehicle;
     private TransLogRecord tlRecord;
 
+    // Where is the current global location of this assigment
+    private double latitudeLocation;
+    private double longitudeLocation;
+
     public CargoAssignment(final String cargoAssignmentId, final String tenantId, Vehicle vehicle, TransLogRecord tlRecord) {
         super(cargoAssignmentId, tenantId);
         this.vehicle = vehicle;
--- sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Patio.java
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Patio.java
@@ -7,6 +7,10 @@ import lombok.Setter;
 @Getter
 public class Patio extends TmsBasicModel {
 
+    // Patio is featuring a geolocalization as well
+    private double latitudeLocation;
+    private double longitudeLocation;
+
     public Patio(final String patioId, final String tenantId) {
         super(patioId, tenantId);
     }
