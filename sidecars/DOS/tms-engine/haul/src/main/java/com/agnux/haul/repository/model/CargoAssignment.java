2ba3504ab1a93f875facc8c06314f0f9c7ada5eb
Edwin Plauchu <j4nusx@yahoo.com>
2025-04-28 20:36:45 -0600

    Patio

--- sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/CargoAssignment.java
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/CargoAssignment.java
@@ -1,8 +1,10 @@
 package com.agnux.haul.repository.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CargoAssignment extends TmsBasicModel {
 
     private Vehicle vehicle;
