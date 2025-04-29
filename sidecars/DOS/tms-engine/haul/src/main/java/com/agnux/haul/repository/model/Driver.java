0275ad9e480305690463b33a3b454edb614cdb08
Edwin Plauchu <j4nusx@yahoo.com>
2025-04-29 12:43:23 -0600

    Patio

--- sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Driver.java
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/model/Driver.java
@@ -1,10 +1,15 @@
 package com.agnux.haul.repository.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class Driver extends TmsBasicModel {
 
+    private String name;
+    private String licenseNumber;
+    
     public Driver(final String DriverId, final String tenantId) {
         super(DriverId, tenantId);
     }
