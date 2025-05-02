c1698d6976540eec7ab64bb0d0d0e8733e7238ce
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-02 13:49:23 -0500

    guice

---
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/core/config/HaulModule.java
@@ -0,0 +1,24 @@
+package com.agnux.haul.core.config;
+
+import com.agnux.haul.core.mgmt.HaulMgmt;
+import com.agnux.haul.repository.IHaulRepo;
+import com.agnux.haul.repository.IHaulRepoImpl;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class HaulModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        // Bind the interface to its implementation if known
+        bind(IHaulRepo.class).to(IHaulRepoImpl.class).in(Singleton.class);
+    }
+
+    @Provides
+    @Singleton
+    public HaulMgmt provideHaulMgmt(IHaulRepo haulRepo) {
+        return new HaulMgmt(haulRepo);
+    }
+}
+
---
+++ sidecars/DOS/tms-engine/haul/src/main/java/com/agnux/haul/repository/IHaulRepoImpl.java
@@ -0,0 +1,25 @@
+package com.agnux.haul.repository;
+
+import com.agnux.haul.repository.model.Agreement;
+import com.agnux.haul.repository.model.CargoAssignment;
+import com.agnux.haul.repository.model.Vehicle;
+
+public class IHaulRepoImpl implements IHaulRepo {
+
+    @Override
+    public String createCargoAssignment(CargoAssignment t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Vehicle getAvailableVehicule(String vehicleIdRef) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Agreement getAvailableAgreement(String agreementRef) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+}
