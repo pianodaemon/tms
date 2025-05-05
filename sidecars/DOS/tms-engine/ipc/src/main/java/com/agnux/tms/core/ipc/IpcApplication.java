59813b9271b836339d395db76a54c9f60beef0f2
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-05 08:38:23 -0500

    spring autogenerated

---
+++ sidecars/DOS/tms-engine/ipc/src/main/java/com/agnux/tms/core/ipc/IpcApplication.java
@@ -0,0 +1,13 @@
+package com.agnux.tms.core.ipc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpcApplication.class, args);
+	}
+
+}
---
+++ sidecars/DOS/tms-engine/ipc/src/test/java/com/agnux/tms/core/ipc/IpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnux.tms.core.ipc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
