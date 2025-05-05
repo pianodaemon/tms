f573cab2dfc2156cf6634c0a8b54bb0b80d80dc9
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-04 21:08:02 -0500

    refactor

--- sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/TransLogRecord.java
+++ sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/TransLogRecord.java
@@ -8,16 +8,22 @@ import lombok.Getter;
 @Getter
 public class TransLogRecord extends TmsBasicModel {
 
-    private final UUID cargoAssignmentId;
-    private final DistUnit distUnit;
-    private final BigDecimal distScalar;
-    private final BigDecimal fuelConsumption;
+    private UUID cargoAssignmentId;
+    private DistUnit distUnit;
+    private BigDecimal distScalar;
+    private BigDecimal fuelConsumption;
 
-    public TransLogRecord(UUID transLogRecordId, UUID tenantId, DistUnit distUnit, UUID cargoAssignmentId, BigDecimal distScalar, BigDecimal fuelConsumption) {
-        super(transLogRecordId, tenantId);
+    public TransLogRecord(final UUID transLogRecordId, final UUID tenantId,
+            final DistUnit distUnit, final UUID cargoAssignmentId,
+            final BigDecimal distScalar, final BigDecimal fuelConsumption) {
+        this(transLogRecordId, tenantId);
         this.cargoAssignmentId = cargoAssignmentId;
         this.distUnit = distUnit;
         this.distScalar = distScalar;
         this.fuelConsumption = fuelConsumption;
+    }
+
+    public TransLogRecord(UUID transLogRecordId, UUID tenantId) {
+        super(transLogRecordId, tenantId);
     }
 }
