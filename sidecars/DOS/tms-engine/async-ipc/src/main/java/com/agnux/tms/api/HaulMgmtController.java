4c8af3424902a1d938c5507e7c68ef62c3d963c6
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-06 08:27:54 -0500

    HaulMgmtController

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/HaulMgmtController.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/HaulMgmtController.java
@@ -1,6 +1,8 @@
 package com.agnux.tms.api;
 
 import com.agnux.tms.core.mgmt.HaulMgmt;
+import com.agnux.tms.core.mgmt.TenantDetailsDto;
+import com.agnux.tms.core.mgmt.TripDetailsDto;
 import com.agnux.tms.errors.TmsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,23 +18,36 @@ public class HaulMgmtController {
 
     private final HaulMgmt haulMgmt;
 
-    @PostMapping("/assign-trip")
-    public Mono<ResponseEntity<UUID>> assignTrip(@RequestBody AssignTripRequest request) {
-        return Mono.fromCallable(() -> {
-            UUID result = haulMgmt.assignTrip(request.getTenant(), request.getTrip());
-            return ResponseEntity.ok(result);
-        }).onErrorResume(TmsException.class, ex
-                -> Mono.just(ResponseEntity.badRequest().build())
-        );
+    @PostMapping("/{tenantId}/{userId}/assign-trip")
+    public Mono<ResponseEntity<String>> assignTrip(
+            @PathVariable UUID tenantId,
+            @PathVariable String userId,
+            @RequestBody TripDetailsDto tripDetails
+    ) {
+        TenantDetailsDto tenantDetails = new TenantDetailsDto(tenantId, userId);
+        return Mono.fromCallable(() -> haulMgmt.assignTrip(tenantDetails, tripDetails))
+                .map(cargoId -> ResponseEntity.ok(cargoId.toString()))
+                .onErrorResume(TmsException.class,
+                        ex -> Mono.just(ResponseEntity.badRequest().body("Error: " + ex.getMessage())));
     }
 
-    @PostMapping("/estimate-fuel")
-    public Mono<ResponseEntity<String>> estimateFuel(@RequestBody AssignTripRequest request) {
+    @GetMapping("/{tenantId}/{userId}/estimate-fuel")
+    public Mono<ResponseEntity<String>> estimateFuel(
+            @PathVariable UUID tenantId,
+            @PathVariable String userId,
+            @RequestParam UUID vehicleId,
+            @RequestParam UUID agreementId,
+            @RequestParam UUID driverId
+    ) {
+        TenantDetailsDto tenant = new TenantDetailsDto(tenantId, userId);
+        TripDetailsDto trip = new TripDetailsDto(vehicleId, agreementId, driverId);
+
         return Mono.fromCallable(()
-                -> haulMgmt.estimateFuel(request.getTenant(), request.getTrip()).toPlainString()
+                -> haulMgmt.estimateFuel(tenant, trip).toPlainString()
         ).map(ResponseEntity::ok)
                 .onErrorResume(TmsException.class, ex
                         -> Mono.just(ResponseEntity.badRequest().body("Error: " + ex.getMessage()))
                 );
     }
+
 }
