9e24b457a1959cdbc6844670056bab22d389703c
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-18 06:47:08 -0500

    Revert "Service package (#21)"
    This reverts commit f192d3e87a3d9baabeb1526ec4d4bc87f91e6c8e.

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/PatioHandler.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/PatioHandler.java
@@ -1,6 +1,5 @@
 package com.agnux.tms.api.handler;
 
-import com.agnux.tms.api.service.PatioService;
 import com.agnux.tms.repository.model.Patio;
 import org.springframework.stereotype.Component;
 import lombok.extern.log4j.Log4j2;
