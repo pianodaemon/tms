36964c9e9d8509bacd08234e26558cc79dcf15dc
Edwin Plauchu <pianodaemon@gmail.com>
2025-05-18 07:07:10 -0600

    Driver - First surname (#22)

--- sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/Driver.java
+++ sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/Driver.java
@@ -12,11 +12,14 @@ import lombok.NoArgsConstructor;
 public class Driver extends TmsBasicModel {
 
     private String name;
+    private String firstSurname;
     private String licenseNumber;
 
-    public Driver(final UUID driverId, final UUID tenantId, final String name, final String licenseNumber) {
+    public Driver(final UUID driverId, final UUID tenantId,
+            final String name, final String firstSurname, final String licenseNumber) {
         this(driverId, tenantId);
         this.name = name;
+        this.firstSurname = firstSurname;
         this.licenseNumber = licenseNumber;
     }
 
