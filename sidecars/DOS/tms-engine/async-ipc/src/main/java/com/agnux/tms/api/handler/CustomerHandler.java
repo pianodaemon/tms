38b68e80ac48ff7d436efb37dd7891736bfb92f3
Edwin Plauchu <pianodaemon@gmail.com>
2025-05-18 05:32:40 -0600

    GenCrudService (#20)

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/CustomerHandler.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/CustomerHandler.java
@@ -1,44 +1,14 @@
 package com.agnux.tms.api.handler;
 
-import java.util.UUID;
-
 import com.agnux.tms.repository.model.Customer;
-import com.agnux.tms.errors.TmsException;
-import com.agnux.tms.repository.IHaulRepo;
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Component;
-
 import lombok.extern.log4j.Log4j2;
 
 @Component
-@RequiredArgsConstructor
 @Log4j2
 public class CustomerHandler extends GenCrudHandler<Customer> {
 
-    private final IHaulRepo repo;
-
-    @Override
-    protected UUID createEntity(Customer entity) throws TmsException {
-        UUID id = repo.createCustomer(entity);
-        log.info("created customer with UUID: " + id);
-        return id;
-    }
-
-    @Override
-    protected Customer getEntity(UUID id) throws TmsException {
-        return repo.getCustomer(id);
-    }
-
-    @Override
-    protected UUID updateEntity(Customer entity) throws TmsException {
-        UUID id = repo.editCustomer(entity);
-        log.info("updated customer with UUID: " + id);
-        return id;
-    }
-
-    @Override
-    protected void deleteEntity(UUID id) throws TmsException {
-        repo.deleteCustomer(id);
+    public CustomerHandler(CustomerService service) {
+        super(service);
     }
 }
---
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/PatioService.java
@@ -0,0 +1,44 @@
+package com.agnux.tms.api.handler;
+
+import com.agnux.tms.errors.TmsException;
+import com.agnux.tms.repository.IHaulRepo;
+import com.agnux.tms.repository.model.Patio;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class PatioService implements GenCrudService<Patio> {
+
+    private final IHaulRepo repo;
+
+    @Override
+    public UUID create(Patio entity) throws TmsException {
+        UUID id = repo.createPatio(entity);
+        log.info("Created patio with UUID: " + id);
+        return id;
+    }
+
+    @Override
+    public Patio read(UUID id) throws TmsException {
+        return repo.getPatio(id);
+    }
+
+    @Override
+    public UUID update(Patio entity) throws TmsException {
+        UUID id = repo.editPatio(entity);
+        log.info("Updated patio with UUID: " + id);
+        return id;
+    }
+
+    @Override
+    public void delete(UUID id) throws TmsException {
+        repo.deletePatio(id);
+        log.info("Deleted patio with UUID: {}", id);
+    }
+}
