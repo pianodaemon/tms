53a92b0294e450e22b74b2ea9cba95f7ab963969
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-25 10:55:26 -0500

    Agreement pagination (fix)

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/AgreementHandler.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/AgreementHandler.java
@@ -1,16 +1,16 @@
 package com.agnux.tms.api.handler;
 
-import com.agnux.tms.api.service.CustomerService;
-import com.agnux.tms.repository.model.Customer;
+import com.agnux.tms.api.service.AgreementService;
+import com.agnux.tms.repository.model.Agreement;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
 
 @Component
 @Log4j2
-public class AgreementHandler extends GenCrudHandler<Customer> {
+public class AgreementHandler extends GenCrudHandler<Agreement> {
 
-    public AgreementHandler(CustomerService service) {
+    public AgreementHandler(AgreementService service) {
         super(service);
     }
 }
