b4abceeb9c218104a960556db5b08150af036e13
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-27 17:27:16 -0600

    simplification

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/dto/AgreementDto.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/dto/AgreementDto.java
@@ -1,5 +1,27 @@
 package com.agnux.tms.api.dto;
 
+import com.agnux.tms.repository.model.DistUnit;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AgreementDto {
 
+    private UUID customerId;
+    private String receiver;     // AKA el destinatario
+    private double latitudeOrigin;
+    private double longitudeOrigin;
+    private double latitudeDestiny;
+    private double longitudeDestiny;
+
+    private DistUnit distUnit;
+    private BigDecimal distScalar;
 }
