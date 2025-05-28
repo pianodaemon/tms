b11ff6a227ddd7fbdc4a510ec251f71f8daee683
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-27 19:35:39 -0500

    MT

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/ScaffoldHandler.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/handler/ScaffoldHandler.java
@@ -8,6 +8,7 @@ import com.agnux.tms.repository.model.TmsBasicModel;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.Collectors;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,43 +17,17 @@ import java.util.UUID;
 import org.springframework.util.MultiValueMap;
 import com.agnux.tms.api.service.CrudService;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import org.springframework.web.reactive.function.server.ServerRequest;
 
-public abstract class ScaffoldHandler<T extends TmsBasicModel, D> implements CrudHandler<ServerRequest, Mono<ServerResponse>> {
+public abstract class ScaffoldHandler<T extends TmsBasicModel, D> extends AbstractCrudHandler<T, D> implements CrudHandler<ServerRequest, Mono<ServerResponse>> {
 
-    protected final CrudService<T> service;
-    protected final Class<D> dtoClazz;
-
-    protected static final ConcurrentMap<Class<?>, Type> typeCache = new ConcurrentHashMap<>();
-    
     protected abstract T entMapper(D dto, UUID tenantId);
-    
+
     protected abstract D dtoMapper(T ent);
 
     @SuppressWarnings("unchecked")
     public ScaffoldHandler(CrudService<T> service) {
-        this.service = service;
-        this.dtoClazz = (Class<D>) extractGenericType();
-    }
-
-    private Type extractGenericType() {
-        return typeCache.computeIfAbsent(getClass(), cls -> {
-            Type type = cls.getGenericSuperclass();
-            Class<?> current = cls;
-            while (!(type instanceof ParameterizedType pt)) {
-                current = current.getSuperclass();
-                type = current.getGenericSuperclass();
-            }
-            return pt.getActualTypeArguments()[1]; // [1] for D
-        });
+        super(service);
     }
 
     @Override
---
+++ sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/api/handler/AbstractCrudHandler.java
@@ -0,0 +1,35 @@
+package com.agnux.tms.api.handler;
+
+import com.agnux.tms.api.service.CrudService;
+import com.agnux.tms.repository.model.TmsBasicModel;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public abstract class AbstractCrudHandler<T extends TmsBasicModel, D> {
+
+    protected final CrudService<T> service;
+    protected final Class<D> dtoClazz;
+
+    protected static final ConcurrentMap<Class<?>, Type> typeCache = new ConcurrentHashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public AbstractCrudHandler(CrudService<T> service) {
+        this.service = service;
+        this.dtoClazz = (Class<D>) extractGenericType();
+    }
+
+    private Type extractGenericType() {
+        return typeCache.computeIfAbsent(getClass(), cls -> {
+            Type type = cls.getGenericSuperclass();
+            Class<?> current = cls;
+            while (!(type instanceof ParameterizedType pt)) {
+                current = current.getSuperclass();
+                type = current.getGenericSuperclass();
+            }
+            return pt.getActualTypeArguments()[1]; // [1] for D
+        });
+    }
+}
