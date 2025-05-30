37a4549223d89a2e19ef1342c979ee9b1ddb1780
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-30 17:11:25 -0600

    refactor

--- sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/config/SecurityConfig.java
+++ sidecars/DOS/tms-engine/async-ipc/src/main/java/com/agnux/tms/api/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.agnux.tms.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @Profile("!test")
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveJwtDecoder jwtDecoder) {
@@ -20,16 +24,13 @@ public class SecurityConfig {
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtDecoder(jwtDecoder)
-                        )
+                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder))
                 )
                 .build();
     }
 
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
-        String jwkSetUri = "https://your-cognito-domain/.well-known/jwks.json"; // Replace with your actual URI
         return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 }
