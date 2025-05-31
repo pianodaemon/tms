d96a7e53c659fa94541ffae7ee0a21237a98ed2c
Edwin Plauchu <j4nusx@yahoo.com>
2025-05-31 15:13:43 -0600

    Patio's name validation

--- sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/Patio.java
+++ sidecars/DOS/tms-engine/elemental/src/main/java/com/agnux/tms/repository/model/Patio.java
@@ -1,6 +1,10 @@
 package com.agnux.tms.repository.model;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
+
+import com.agnux.tms.errors.ErrorCodes;
+import com.agnux.tms.errors.TmsException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -16,14 +20,47 @@ public class Patio extends TmsBasicModel {
     private double latitudeLocation;
     private double longitudeLocation;
 
+    private static final Pattern NAME_FIELD_MULTI_CONSECUTIVE = Pattern.compile("\\.\\.+|--++");
+    private static final Pattern NAME_FIELD_INVALID_SEQUENCES = Pattern.compile("-\\.|\\.-");
+
     public Patio(final UUID patioId, final UUID tenantId, String name, double latitudeLocation, double longitudeLocation) {
         this(patioId, tenantId);
-        this.name = name;
+        this.name = removeMultipleSpaces(name.trim());
         this.latitudeLocation = latitudeLocation;
         this.longitudeLocation = longitudeLocation;
     }
 
     public Patio(final UUID patioId, final UUID tenantId) {
         super(patioId, tenantId);
+    }
+
+    @Override
+    public void validate() throws TmsException {
+        super.validate();
+        this.validateName();
+    }
+
+    public void validateName() throws TmsException {
+        if (name == null || name.isBlank()) {
+            throw new TmsException("Patio name must not be null or blank", ErrorCodes.INVALID_DATA);
+        }
+
+        if (name.startsWith(".") || name.startsWith("-")) {
+            throw new TmsException("Patio name must not start with a dot", ErrorCodes.INVALID_DATA);
+        }
+
+        if (NAME_FIELD_INVALID_SEQUENCES.matcher(name).find()) {
+            throw new TmsException("Patio name must not contain the sequences '&.' or '.&'", ErrorCodes.INVALID_DATA);
+        }
+
+        if (NAME_FIELD_MULTI_CONSECUTIVE.matcher(name).find()) {
+            throw new TmsException("Patio name must not contain multiple consecutive dots", ErrorCodes.INVALID_DATA);
+        }
+
+        for (char c : name.toCharArray()) {
+            if (!(Character.isLetterOrDigit(c) || c == ' ' || c == '.' || c == '-')) {
+                throw new TmsException("Patio name must be alphanumeric and may contain a few single special characters only", ErrorCodes.INVALID_DATA);
+            }
+        }
     }
 }
