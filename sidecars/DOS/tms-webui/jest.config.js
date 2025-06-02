b506d33b6988a73532663fe8b2310108663720e4
Edwin Plauchu <pianodaemon@gmail.com>
2025-06-01 21:54:24 -0600

    Update jest.config.js

--- sidecars/DOS/tms-webui/jest.config.js
+++ sidecars/DOS/tms-webui/jest.config.js
@@ -11,5 +11,14 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.ts", "!**/*.d.ts"],
-  coverageReporters: ["json", "json-summary", "text", "lcov"]
+  coverageReporters: ["json", "json-summary", "text", "lcov"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "tests/", // or wherever your tests live
+  ],
+  // map coverage back to source with source maps
+  mapCoverage: true,
+  // This ensures coverage is collected from files that are imported in tests
+  coverageProvider: "v8",
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
 };
