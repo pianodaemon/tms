6140447b9c2b3393963eecaacc8f6b997b37d198
Edwin Plauchu <pianodaemon@gmail.com>
2025-06-08 11:30:19 -0600

    Up2date (#60)

--- sidecars/DOS/tms-webui/src/pages/Home.tsx
+++ sidecars/DOS/tms-webui/src/pages/Home.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../App.css'
-
-import SimulationGrid from '../comps/paginator/SimulationGrid.tsx'
+import SimulationGrid from '../comps/paginator/SimulationGrid'
 
 function Home() {
   const [count, setCount] = useState(0)
@@ -30,7 +29,7 @@ function Home() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-<SimulationGrid />
+      <SimulationGrid />
     </>
   )
 }
