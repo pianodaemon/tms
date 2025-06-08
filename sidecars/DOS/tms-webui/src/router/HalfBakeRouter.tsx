e1ad03b1622ec6d2e671396f2e8675f10ac63443
Edwin Plauchu <j4nusx@yahoo.com>
2025-06-08 09:59:44 -0600

    configured

--- sidecars/DOS/tms-webui/src/router/HalfBakeRouter.tsx
+++ sidecars/DOS/tms-webui/src/router/HalfBakeRouter.tsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 
 function HalfBakeRouter() {
 
+  const homePath: string = "/home";
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-     
+      <Route path={homePath} element={<Home />} />
+
+      <Route path="/*" element={<Navigate to={homePath} replace />} />
     </Routes>
   );
 }
