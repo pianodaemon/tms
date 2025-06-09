da6eb0deffd3c7ef66c493116558619d35e9f705
Edwin Plauchu <j4nusx@yahoo.com>
2025-06-09 10:01:14 -0600

    error_handling

---
+++ sidecars/DOS/tms-webui/src/icskit/ehandle.ts
@@ -0,0 +1,12 @@
+function catchWithExpError<T, E extends new (...args: any[]) => Error>(
+  promise: Promise<T>,
+  errorsToCatch?: E[]
+): Promise<[undefined, T] | [InstanceType<E>]> {
+
+  return promise.then((data) => [undefined, data] as [undefined, T])
+    .catch(error  => {
+      if (!errorsToCatch || errorsToCatch.some((ErrType) => error instanceof ErrType)) return [error];
+
+      throw error; // Re-throw unexpected errors
+  }); 
+}
